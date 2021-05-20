22c0fc0c36eff50d5686f669747299e45bb54643
Alex Young <alexyoung23j@gmail.com>
2021-05-19 23:40:12 -0700

    Fixed css problem

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,24 @@ import CommandView from './components/command_window/CommandView'
 import SettingsView from './components/SettingsView'
 const { ipcRenderer } = require('electron')
 
-//import "./css/EditorComponent.css"
+// Access to terminal console log
+var nodeConsole = require('console');
+var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+
+// CSS for Editor Components defined here... I know this is hilarious 
+const stringifiedCSS = `
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+        .commandLineStyle {
+          font-family: 'Montserrat', sans-serif;
+          font-size: 24px;
+          min-width: 200px;
+        }
+
+        .textSnippetStyle {
+          font-family: 'Times', sans-serif;
+          font-size: 12px;
+        }`
 
 
 
@@ -17,7 +34,12 @@ function createNewWindow(
   currentWindowType: WindowType,
   containerEl: HTMLDivElement
 ) {
+
   const externalWindow = window.open('', currentWindowType)
+
+  // Inject css into new window
+  const addStylesString = `<html><head><style>${stringifiedCSS}</style></head></html>`
+  externalWindow.document.write(addStylesString)
 
   // Append the container div 
   if (externalWindow?.document != null) {
@@ -26,7 +48,6 @@ function createNewWindow(
 
   return externalWindow
 }
-
 
 
 function App() {
