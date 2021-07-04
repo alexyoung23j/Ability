a87de0131dd01ed6bd0656ce5576b1ccfd5fdec5
Kedar Rudrawar <rudrawarkedar@gmail.com>
2021-07-04 09:56:28 -0700

    add todo and prettify main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,9 @@ import path from 'path';
 import os from 'os';
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
- 
+
 // TODO: This isnt fixing anyting
-ipcMain.setMaxListeners(Infinity)
+ipcMain.setMaxListeners(Infinity);
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -37,7 +37,7 @@ const createSentinelWindow = (): void => {
   const display = screen.getPrimaryDisplay();
   const maxiSize = display.workAreaSize;
 
-  // These are the default settings for the command view. May end up being something different in production 
+  // These are the default settings for the command view. May end up being something different in production
   sentinelWindow = new BrowserWindow({
     frame: false,
     height: maxiSize.height,
@@ -69,7 +69,7 @@ const createSentinelWindow = (): void => {
       sentinelWindow.setOpacity(1);
     }, 100);
   });
-  
+
   sentinelWindow.on('hide', () => {
     sentinelWindow.setOpacity(0);
   });
@@ -103,6 +103,7 @@ const createTray = () => {
 /// -------------------------- APP EVENT HANDLERS -------------------- ///
 
 // Setup when app launches
+// TODO: set to only run this on DEBUG mode
 const reactDevToolsPath = path.join(
   os.homedir(),
   '/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.13.5_0'
@@ -136,55 +137,54 @@ app.on('activate', () => {
   }
 });
 
-
 /// ------------------------- IPC LISTENERS ------------------------ ///
 
 // Hide the Command Line
 ipcMain.on('command-line-native-blur', () => {
-  windowDisplayHandler('COMMMAND', true)
-  sentinelWindow.hide()
+  windowDisplayHandler('COMMMAND', true);
+  sentinelWindow.hide();
 });
 
 // Close the settings window (with a button)
 ipcMain.on('close-settings', () => {
-  sentinelWindow.hide()
-  windowDisplayHandler('COMMAND', false)
-})
+  sentinelWindow.hide();
+  windowDisplayHandler('COMMAND', false);
+});
 
 // Update our "state" to indicate the command line is currently in the sentinelWindow
 ipcMain.on('command-showing', () => {
-  currentWindow = "COMMAND"
-})
+  currentWindow = 'COMMAND';
+});
 
 // Update our "state" to indicate the settings view is currently in the sentinelWindow
 ipcMain.on('settings-showing', () => {
-  currentWindow = "SETTINGS"
-})
+  currentWindow = 'SETTINGS';
+});
 
 /// ----------------------------- OTHER METHODS ------------------- ///
 
 // Handles global key shortcuts (incomplete, will add parametrized behavior)
 function keyboardShortcutHandler() {
-  if (currentWindow == "SETTINGS") {
-    sentinelWindow.hide()
-    windowDisplayHandler("COMMAND", false)
+  if (currentWindow == 'SETTINGS') {
+    sentinelWindow.hide();
+    windowDisplayHandler('COMMAND', false);
   } else {
     if (sentinelWindow.isVisible()) {
-      sentinelWindow.hide()
+      sentinelWindow.hide();
     } else {
-      sentinelWindow.show()
+      sentinelWindow.show();
     }
   }
 }
 
 // Open Command Line (wrapper for future implementation needs)
 function openCommandLine() {
-  windowDisplayHandler("COMMAND", true)
+  windowDisplayHandler('COMMAND', true);
 }
 
 // Open Settings View (wrapper for future implementation needs)
 function openSettingsView() {
-  windowDisplayHandler("SETTINGS", true)
+  windowDisplayHandler('SETTINGS', true);
 }
 
 // Handles hiding and showing
@@ -193,29 +193,27 @@ function windowDisplayHandler(toShow: string, showAfterChanges) {
   const maxiSize = display.workAreaSize;
 
   // Show Command Line and update window settings
-  if (toShow === "COMMAND") {
-    sentinelWindow.webContents.send('show-command', 'display command line')
-    sentinelWindow.setSize(maxiSize.width, maxiSize.height)
-    sentinelWindow.setBounds({x: 0, y: 0})
-    sentinelWindow.setResizable(false)
-    sentinelWindow.setMovable(false)
-    sentinelWindow.setHasShadow(false)
+  if (toShow === 'COMMAND') {
+    sentinelWindow.webContents.send('show-command', 'display command line');
+    sentinelWindow.setSize(maxiSize.width, maxiSize.height);
+    sentinelWindow.setBounds({ x: 0, y: 0 });
+    sentinelWindow.setResizable(false);
+    sentinelWindow.setMovable(false);
+    sentinelWindow.setHasShadow(false);
 
     // Show Settings and update window settings
-  } else if (toShow === "SETTINGS") {
-    sentinelWindow.webContents.send('show-settings', 'display settings')
-    sentinelWindow.setSize(maxiSize.width/2, maxiSize.height)
-    sentinelWindow.setBounds({x: 0, y: 0})
-    sentinelWindow.setResizable(true)
-    sentinelWindow.setMovable(true)
-    sentinelWindow.setHasShadow(true)
-    sentinelWindow.center()
-    
+  } else if (toShow === 'SETTINGS') {
+    sentinelWindow.webContents.send('show-settings', 'display settings');
+    sentinelWindow.setSize(maxiSize.width / 2, maxiSize.height);
+    sentinelWindow.setBounds({ x: 0, y: 0 });
+    sentinelWindow.setResizable(true);
+    sentinelWindow.setMovable(true);
+    sentinelWindow.setHasShadow(true);
+    sentinelWindow.center();
   }
 
   // Show the window after changes (only relevant if window hidden before change)
   if (showAfterChanges) {
-    sentinelWindow.show()
+    sentinelWindow.show();
   }
-
 }
