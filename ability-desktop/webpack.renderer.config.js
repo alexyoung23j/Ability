b96dc7cc37412cd55042181cc5122f6c91830ded
Kedar Rudrawar <rudrawarkedar@gmail.com>
2021-10-24 22:54:49 -0700

    remove useless import

--- ability-desktop/webpack.renderer.config.js
+++ ability-desktop/webpack.renderer.config.js
@@ -1,6 +1,5 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
-const path = require('path');
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 
 rules.push({
