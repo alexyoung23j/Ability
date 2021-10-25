c0ec50207f55e1988fe7e55971408866dc454c99
Kedar Rudrawar <rudrawarkedar@gmail.com>
2021-10-24 18:18:54 -0700

    add global firebase-tools install

--- install_deps.sh
+++ install_deps.sh
@@ -3,8 +3,11 @@ if [[ -z "${ABILITY_HOME}" ]]; then
    source ./activate.sh
 fi
 
+yarn global add firebase-tools
+
 cd $ABILITY_HOME/ability-web/
 yarn
+cd $ABILITY_HOME/ability-web/
 cd $ABILITY_HOME/ability-desktop/
 yarn
 cd $ABILITY_HOME 
